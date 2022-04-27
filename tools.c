edf19498634b9c4bc81278f7c22d184061d35309
lilifus <zakaria.ejjed@gmail.com>
2022-04-27 20:57:14 +0200

    mul_and_mod deleted

--- tools.c
+++ tools.c
@@ -1,6 +1,6 @@
 #include "tools.h"
 
-//return 1 if n is found, 0 if not, -1 if error
+//return 1 if n is found, 0 if not
 int check_tab(mpz_t *tab, int size, mpz_t n){
     for(int i=0; i<size; i++){
         if(mpz_cmp(tab[i], n) == 0){
@@ -20,18 +20,7 @@ void free_tab(mpz_t *tab, int size){
     free(tab);
 }
 
-// return : result = a * b mod n
-void mul_and_mod(mpz_t a, mpz_t b, mpz_t n, mpz_t result){
-    mpz_t r;
-    mpz_init(r);
-    mpz_mul(r, a, b);
-    mpz_mod(r, r, n);
-    mpz_set(result, r);
-    mpz_clear(r);
-}
-
-
-// Check if a string is a number
+// Check if a string is a number (return 1 if yes, 0 if not)
 int isnumber(char *str){
     int i = 0;
     while(str[i] != '\0'){
--- tools.h
+++ tools.h
@@ -1,4 +1,4 @@
-#include "gmp.h"
+#include <gmp.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -6,5 +6,4 @@
 
 int check_tab(mpz_t *tab, int size, mpz_t n);
 void free_tab(mpz_t *tab, int size);
-void mul_and_mod(mpz_t a, mpz_t b, mpz_t n, mpz_t result);
 int isnumber(char *str);
